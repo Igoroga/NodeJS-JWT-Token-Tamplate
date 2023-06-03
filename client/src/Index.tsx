fad62dfcb4cb856cfe8c5067f2b29b7d8a4e6a73
Igor Gusakov <igorgusakov199918@gmail.com>
2023-06-04 00:57:18 +0600

    test

--- client/src/Index.tsx
+++ client/src/Index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { BrowserRouter } from 'react-router-dom'
-// import {App} from './App';
-// import { GlobalStyles } from './globalStyle';
-// import { Provider } from 'react-redux'
-// import { store } from './store/store';
+import { BrowserRouter } from 'react-router-dom'
+import {App} from './App';
+import { GlobalStyles } from './globalStyle';
+import { Provider } from 'react-redux'
+import { store } from './store/store';
 import './forStyle/fonts/SFProDisplay-Medium.woff2'
 import './forStyle/fonts/SFProDisplay-Regular.woff2'
 import './index.css'
